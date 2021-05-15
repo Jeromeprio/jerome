829e8c5caa56582a7c4a92e907f6e8a05d2b5ac9
Jeromeprio <jeromepriolet@gmail.com>
2021-05-15 10:32:39 +0200

    import map

---
+++ déco1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="déco1" tilewidth="16" tileheight="16" tilecount="1056" columns="24">
+ <image source="Tiles/audimax.png" width="384" height="704"/>
+</tileset>
